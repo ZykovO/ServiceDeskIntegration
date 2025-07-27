261a142aff6c90ffa4309d2decb54af058fbab5a
a.zykov <a.zykov@pc-service.ua>
2025-07-27 17:08:06 +0300

    + test page width3

--- src/app/home-page/home-page.ts
+++ src/app/home-page/home-page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {TelegramService} from '../services/telegram';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
@@ -7,5 +9,22 @@ import { Component } from '@angular/core';
   styleUrl: './home-page.css'
 })
 export class HomePage {
+  constructor(
+    private telegram: TelegramService,
+    private router: Router
+  ) {
+    // получаем конкретный продукт из сервиса
+    this.goBack = this.goBack.bind(this);
+  }
 
+
+  goBack() {
+    this.router.navigate(['/']);
+  }
+
+  ngOnInit(): void {
+    this.telegram.BackButton.show();
+    // добавляем функционал для перехода назад в телеграм
+    this.telegram.BackButton.onClick(this.goBack);
+  }
 }
