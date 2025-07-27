64b77612cec7b2dd5c679c2b5530a84a0c11cf7a
a.zykov <a.zykov@pc-service.ua>
2025-07-27 16:57:17 +0300

    + test page width

---
+++ src/app/home-page/home-page.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-page',
+  imports: [],
+  templateUrl: './home-page.html',
+  styleUrl: './home-page.css'
+})
+export class HomePage {
+  ngOnInit() {
+    const tg = window.Telegram?.WebApp;
+    if (tg) {
+      console.log(tg.viewportStableWidth);
+    }
+
+  }
+
+}
