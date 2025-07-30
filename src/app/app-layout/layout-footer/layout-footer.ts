e794cdc191c24b153576af5f1fe9874d50b9582e
a.zykov <a.zykov@pc-service.ua>
2025-07-30 14:56:32 +0300

    - бургер кнопка сайдбара

--- src/app/app-layout/layout-footer/layout-footer.ts
+++ src/app/app-layout/layout-footer/layout-footer.ts
@@ -1,22 +1,12 @@
 import { Component } from '@angular/core';
-import {NgForOf} from '@angular/common';
-import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-layout-footer',
   imports: [
-    NgForOf,
-    RouterLink
   ],
   templateUrl: './layout-footer.html',
   styleUrl: './layout-footer.css'
 })
 export class LayoutFooter {
-  footerItems = undefined
-  //   [
-  //   { icon: '🏠', text: 'Home', link: '/' },
-  //   { icon: '🔍', text: 'Search', link: '/search' },
-  //   { icon: '🛒', text: 'Cart', link: '/cart' },
-  //   { icon: '👤', text: 'Profile', link: '/profile' }
-  // ];
+
 }
--- src/app/app-layout/layout-sidebar/layout-sidebar.ts
+++ src/app/app-layout/layout-sidebar/layout-sidebar.ts
@@ -1,22 +1,11 @@
 import { Component } from '@angular/core';
-import {RouterLink} from '@angular/router';
-import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-layout-sidebar',
   imports: [
-    RouterLink,
-    NgForOf
   ],
   templateUrl: './layout-sidebar.html',
   styleUrl: './layout-sidebar.css'
 })
 export class LayoutSidebar {
-  menuItems = undefined
-  //   [
-  //   { icon: '🏠', text: 'Home', link: '/' },
-  //   { icon: '📊', text: 'Dashboard', link: '/dashboard' },
-  //   { icon: '⚙️', text: 'Settings', link: '/settings' },
-  //   { icon: 'ℹ️', text: 'About', link: '/about' }
-  // ];
 }
