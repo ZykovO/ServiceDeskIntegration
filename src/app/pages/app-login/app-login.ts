c7c879a02a6c40d55052ac0cb582595acb0f7446
ZykovO <7CPY6z4k6udv@rJ>
2025-08-04 10:38:27 +0300

    - fix5

--- src/app/pages/app-login/app-login.ts
+++ src/app/pages/app-login/app-login.ts
@@ -15,7 +15,7 @@ import {environment} from '../../../environments/environment';
 })
 export class AppLogin implements OnInit {
   private authService = inject(AuthService);
-  protected telegramService = inject(TelegramService);
+  private telegramService = inject(TelegramService);
   private router = inject(Router);
 
   isLoading = false;
@@ -37,11 +37,11 @@ export class AppLogin implements OnInit {
       const initData = this.telegramService.initData;
       const initDataUnsafe = this.telegramService.initDataUnsafe;
 
-      // this.isTelegramAvailable = !!(initData && initDataUnsafe?.user);
-      //
-      // if (!this.isTelegramAvailable) {
-      //   this.errorMessage = 'Откройте приложение из Telegram';
-      // }
+      this.isTelegramAvailable = !!(initData && initDataUnsafe?.user);
+
+      if (!this.isTelegramAvailable) {
+        this.errorMessage = 'Откройте приложение из Telegram';
+      }
     } catch (error) {
       console.error('Telegram check error:', error);
       this.isTelegramAvailable = false;
