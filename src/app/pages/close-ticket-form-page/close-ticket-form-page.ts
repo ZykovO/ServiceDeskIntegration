3815b1a0e90d84f697e600557f8f93c2e10f9571
ZykovO <a.zykov@pc-service.ua>
2025-08-18 15:18:04 +0300

    + отправка запроса на бэк для получения формы для закрытия заявки

--- src/app/pages/close-ticket-form-page/close-ticket-form-page.ts
+++ src/app/pages/close-ticket-form-page/close-ticket-form-page.ts
@@ -130,7 +130,7 @@ export class CloseTicketFormPage implements OnInit, OnDestroy {
 
     // Здесь будет вызов API для отправки данных
     // Например: this.ticketService.closeTicket(this.requestData)
-
+    this.ticketService.get_preparenew(this.requestData)
     console.log('Отправляем данные:', this.requestData);
 
     // Имитация отправки
--- src/app/services/ticket-service.ts
+++ src/app/services/ticket-service.ts
@@ -3,6 +3,7 @@ import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {environment} from '../../environments/environment';
 import {Ticket} from '../interfaces/ticket.interface';
+import {PrepareNewRequest} from '../interfaces/prepare-new-request';
 
 @Injectable({
   providedIn: 'root'
@@ -15,5 +16,13 @@ export class TicketService {
     return this.http.get<Ticket>(`${this.API_URL}/tickets/${ticket_id}`);
   }
 
-
+  get_preparenew(requestData: PrepareNewRequest) {
+    return this.http.post(`${this.API_URL}/tickets/preparenew`, {
+      contractor_id:requestData.contractor_id,
+      ticket_id:requestData.contractor_id,
+      client_id:requestData.contractor_id,
+      ticket_type:requestData.contractor_id,
+      act_type:requestData.contractor_id,
+    });
+  }
 }
